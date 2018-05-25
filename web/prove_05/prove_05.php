86660d21554abb5f6c67a05662bb428e937d9586
Bryan Lopez <lop00003@byui.edu>
2018-05-25 14:38:51 -0700

    update prove 05

--- web/prove_05/prove_05.php
+++ web/prove_05/prove_05.php
@@ -1,7 +1,3 @@
-<!-- <?php
-require('connect.php');
-
-?> -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
                     "http://www.w3.org/TR/xhtmll/DTD/xhtmll-stict.dtd">
 <html xmlns="http://www.w3.ord/1999/xhtml" lang="en" xml:lang="en" >
