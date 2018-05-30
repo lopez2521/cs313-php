afa097d2e6a8630e6f7fd6fe771e921a8a386161
Bryan Lopez <lop00003@byui.edu>
2018-05-30 12:12:24 -0700

    updating project

--- web/project_01/signupconfirmation.php
+++ web/project_01/signupconfirmation.php
@@ -1,7 +1,5 @@
 <?php
 
-	$email = $_POST['email'];
-	$password = $_POST['password'];
 
 if (empty($_POST["email"])) {
     $emailErr = "Email is required";
@@ -16,11 +14,14 @@ if (empty($_POST["email"])) {
   require('connect.php');
   $db = get_db();
 
+  $email = $_POST['email'];
+	$password = $_POST['password'];
+
   try
 {
 	// Add the Scripture
 	// We do this by preparing the query with placeholder values
-	$query = 'INSERT INTO create_account(user_name, password) VALUES('$email',  '$password')';
+	$query = "INSERT INTO create_account(user_name, password) VALUES('$email',  '$password')";
 	$statement = $db->prepare($query);
 	// Now we bind the values to the placeholders. This does some nice things
 	// including sanitizing the input with regard to sql commands.
@@ -36,6 +37,8 @@ catch (Exception $ex)
 	echo "Error with DB. Details: $ex";
 	die();
 }
+
+echo "You are all signed up!";
 
 	
 ?>
--- web/project_01/welcome_back.php
+++ web/project_01/welcome_back.php
@@ -9,15 +9,25 @@
 	$email - $_POST['email'];
 	$password = $_POST['password'];
 
-	if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
+
+   if (!empty($email)) {
+   	if (!empty($password)) {
+   		require('connect.php');
+   		$db = get_db();
+   	}
+   	if ($email == $_POST['email'] && $password == $_POST['password']) {
+   		echo "Welcome Back" . $email;
+   	}
+   }
+	// if (empty($_POST["email"])) {
+ //    $emailErr = "Email is required";
+ //  } else {
+ //    $email = test_input($_POST["email"]);
+ //    // check if e-mail address is well-formed
+ //    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ //      $emailErr = "Invalid email format"; 
+ //    }
+ //  }
 	?>
 
 
