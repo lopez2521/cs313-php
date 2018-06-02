917f4864614a964c118c5f95ae6864cbed19be93
Bryan Lopez <lop00003@byui.edu>
2018-06-02 09:20:26 -0700

    fixing email insert into

--- web/project_01/signupconfirmation.php
+++ web/project_01/signupconfirmation.php
@@ -1,16 +1,5 @@
 <?php
 
-
-if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
-
   require('connect.php');
   $db = get_db();
 
