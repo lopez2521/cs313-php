a1d7008190538fc2ddaccb5afbca870931093bb3
Bryan Lopez <lop00003@byui.edu>
2018-06-02 11:23:18 -0700

    debugging deleting email

--- web/project_01/removeEmail.php
+++ web/project_01/removeEmail.php
@@ -7,6 +7,56 @@
 <body>
 	<?php
 
+try
+{
+
+	$removeEmail = $_POST['removeEmail'];
+
+	$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stmt = $db->prepare('DELETE FROM email_list WHERE email = :removeEmail');
+$statement->bindValue(':removeEmail', $removeEmail);
+$stmt->execute();
+// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);  // delete doesn't return anything
+// if you need a list to population, you'll need a select
+
+echo '<h1>Email list</h1>';
+
+// you aren't displaying a list here
+}
+catch (PDOException $ex) // or something like this
+{
+	echo "There is a problem."
+}
+
+
+	?>
+
+</body>
+</html>
+
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<title>Remove Email</title>
+	<link rel="stylesheet" type="text/css" href="fan.css">
+</head>
+<body>
+	<?php
+
 	$removeEmail = $_POST['removeEmail'];
 
 	$dbUrl = getenv('DATABASE_URL');
@@ -34,4 +84,4 @@ echo '<h1>Email list</h1>';
 	?>
 
 </body>
-</html>
+</html> -->
