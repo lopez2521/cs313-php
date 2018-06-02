4ee4ba454f1afc2c84846970c34eb4cabe6595d6
Bryan Lopez <lop00003@byui.edu>
2018-06-02 09:47:38 -0700

    trying to reveal a table to the user

--- web/project_01/signupconfirmation.php
+++ web/project_01/signupconfirmation.php
@@ -38,5 +38,39 @@ echo "<h1>You are all signed up!</h1>";
 	
 ?>
 
+<?php
+
+
+
+  require('connect.php');
+  $db = get_db();
+
+  $email = $_POST['email'];
+
+  try
+{
+	
+	$query = 'SELECT * FROM email_list (email) VALUES(:email)';
+	$statement = $db->prepare($query);
+	
+	$statement->bindValue(':email', $email);
+	$statement->execute();
+	
+}
+catch (Exception $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error with DB. Details: $ex";
+	die();
+}
+
+echo "<h1>You are all signed up!</h1>";
+
+	
+?>
+
+
+
 </body>
 </html>
