171ec7dda86e1c160dbdc468563e3bb2532a71e2
Bryan Lopez <lop00003@byui.edu>
2018-06-02 11:29:17 -0700

    adding new remove email form

---
+++ web/project_01/removeemailform.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Remove Email Form</title>
+	<link rel="stylesheet" type="text/css" href="fan.css">
+</head>
+<body>
+
+<p>If you would like to remove you email, then we understand.</p>
+
+<form method="post" id="removeEmail" action="removeEmail.php">
+	<input type="text" name="removeEmail">
+	<input type="submit" name="removedEmail" value="Remove Email">
+</form>
+</body>
+</html>
