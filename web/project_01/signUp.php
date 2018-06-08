96e4c9754e24d380697699d9983237b58d1b91be
Bryan Lopez <lop00003@byui.edu>
2018-06-08 12:48:31 -0700

    added the files

---
+++ web/project_01/signUp.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+</head>
+<body>
+
+    <form id="signup" method="POST" action="signedUp.php">
+	<label for="username">Username</label>
+	<input type="text" name="username">
+	<label for="password">Password:</label>
+	<input type="password" name="password">
+	<input type="submit" name="signUp" value="Sign Up">
+
+    </form>
+</body>
+</html>
