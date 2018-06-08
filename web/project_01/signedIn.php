9ddede2fe237fa7ab9145b5879970551af937174
Bryan Lopez <lop00003@byui.edu>
2018-06-08 13:23:58 -0700

    degug signedIn

--- web/project_01/signedIn.php
+++ web/project_01/signedIn.php
@@ -1,46 +1,34 @@
 <?php
 session_start();
 
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-	
+  
 
 
-	require('connect.php');
-	$db = get_db();
+  require('connect.php');
+  $db = get_db();
     
     $query = 'SELECT password FROM users WHERE username = :username';
 
-    			$statement = $db->prepare($query);
-    			$statement->bindValue(':username', $username);
-    			
-    			$statement->execute();
+          $statement = $db->prepare($query);
+          $statement->bindValue(':username', $username);
+          
+          $statement->execute();
 
-   		$fetch_user = $statement->fetch();
-   		
+      $fetch_user = $statement->fetch();
+      
 
-   		if (password_verify($password, $fetch_user['password'])) {
-   			$_SESSION['username'] = $username;
-   			header('Location: welcome.php');
-   		}
+      if (password_verify($password, $fetch_user['password'])) {
+        $_SESSION['username'] = $username;
+        header('Location: welcome.php');
+      }
 
-   		else {
-   			header('Location: signIn.php');
-   		}
+      else {
+        header('Location: signIn.php');
+      }
 
    
-	?>
+  ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Sign In</title>
-</head>
-<body>
-
-  <br />
-  <p>If you want to go back to the main page, go <a href="main.php">here.</a></p>
-
-</body>
-</html>
