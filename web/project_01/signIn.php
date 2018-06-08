977cb0b0beeb4b5f4aae3da9c9fd9160e7fe2ae4
Bryan Lopez <lop00003@byui.edu>
2018-06-08 13:30:22 -0700

    adding files again

---
+++ web/project_01/signIn.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign In</title>
+</head>
+<body>
+
+<form id="signIn" method="POST" action="signedIn.php">
+	<label for="username">Username</label>
+	<input type="text" name="username">
+	<label for="password">Password:</label>
+	<input type="password" name="password">
+	<input type="submit" name="signIn" value="Sign In">
+</form>
+
+<p><a href="signUp.php">Sign Up</a></p>
+
+
+</body>
+</html>
+
+
